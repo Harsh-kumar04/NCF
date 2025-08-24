e89f525ed73e3624e5224811c23837ecca95eeb1
AJ_GUIN <ashisjguin20@gmail.com>
2025-08-24 18:41:29 +0530

    testimonial added

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -23,8 +23,8 @@ export const testimonials: Testimonial[] = [
   },
   {
     id: 3,
-    name: 'Arpan Abinash (Mayurbhanjia Biker)',
-    role: ' ~ Impact Alliance team member',
+    name: 'Arpan Abinash (Hands for Help - NGO)',
+    role: ' ~ Impact Alliance member',
     avatar: '/award-slides/local_DP.jpg',
     text: 'Learning from a Nobel Citizen was an inspiring and transformative experience that deepened my understanding of the true spirit of Corporate Social Responsibility. Beyond policies and programs, they emphasized that CSR is about empathy, sustainability, and long-term community.',
   },
@@ -38,8 +38,16 @@ export const testimonials: Testimonial[] = [
   {
     id: 5,
     name: 'Tisser Artisan Trust',
-    role: ' ~ Trust',
+    role: ' ~ Impact Alliance member',
     avatar: '/award-slides/local_DP.jpg',
     text: 'The experience was wonderful. Each session was useful',
   },
+  {
+    id: 6,
+    name: 'Asim Khan (Cadence CSR Head)',
+    role: ' ~ Impact Alliance Mentor',
+    avatar: '/award-slides/local_DP.jpg',
+    text: 'Impact alliance is an important initiative for the betterment of NGOs working at the grassroots level in aspect of capacity building and being CSR ready.It also provides an opportunity for young professionals like us to know more about the NGOs working at the grassroots level and the day-to-day challenges they face.',
+  },
+
 ];
